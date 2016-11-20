377281fc12de4e9a7225842f40b6bbb3f2beeb5e
adenw <adenw@DESKTOP-26H5264>
2016-11-20 00:55:55 -0600

    0.9.0 release to nuget

--- Source/RadioThermostat.Api/ClientBase.cs
+++ Source/RadioThermostat.Api/ClientBase.cs
@@ -137,19 +137,26 @@ namespace RadioThermostat.Api
             if (request == null)
                 throw new ArgumentNullException(nameof(request));
 
-            var message = string.Format(
-                Environment.NewLine + "---------------------------------" + Environment.NewLine +
-                "WEB REQUEST to {0}" + Environment.NewLine +
-                "-Method: {1}" + Environment.NewLine +
-                "-Headers: {2}" + Environment.NewLine +
-                "-Contents: " + Environment.NewLine + "{3}" + Environment.NewLine +
-                "---------------------------------",
-                request.RequestUri.OriginalString,
-                request.Method.Method,
-                request.Headers?.ToString(),
-                request.Content?.ReadAsStringAsync().Result
-            );
-            this.Log(message);
+            try
+            {
+                var message = string.Format(
+                    Environment.NewLine + "---------------------------------" + Environment.NewLine +
+                    "WEB REQUEST to {0}" + Environment.NewLine +
+                    "-Method: {1}" + Environment.NewLine +
+                    "-Headers: {2}" + Environment.NewLine +
+                    "-Contents: " + Environment.NewLine + "{3}" + Environment.NewLine +
+                    "---------------------------------",
+                    request.RequestUri.OriginalString,
+                    request.Method.Method,
+                    request.Headers?.ToString(),
+                    request.Content?.ReadAsStringAsync().Result
+                );
+                this.Log(message);
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error during Log(HttpRequestMessage request): " + ex.ToString());
+            }
         }
 
         /// <summary>
@@ -162,21 +169,29 @@ namespace RadioThermostat.Api
                 throw new ArgumentNullException(nameof(response));
             
             this.Log(response.RequestMessage);
-            var message = string.Format(
-                Environment.NewLine + "---------------------------------" + Environment.NewLine +
-                "WEB RESPONSE to {0}" + Environment.NewLine +
-                "-HttpStatus: {1}" + Environment.NewLine +
-                "-Reason Phrase: {2}" + Environment.NewLine +
-                "-ContentLength: {3:0.00 KB}" + Environment.NewLine +
-                "-Contents: " + Environment.NewLine + "{4}" + Environment.NewLine +
-                "---------------------------------",
-                response.RequestMessage.RequestUri.OriginalString,
-                string.Format("{0} {1}", (int)response.StatusCode, response.StatusCode.ToString()),
-                response.ReasonPhrase,
-                Convert.ToDecimal(Convert.ToDouble(response.Content.Headers.ContentLength) / 1024),
-                response.Content?.ReadAsStringAsync().Result
-                );
-            this.Log(message);
+
+            try
+            {
+                var message = string.Format(
+                    Environment.NewLine + "---------------------------------" + Environment.NewLine +
+                    "WEB RESPONSE to {0}" + Environment.NewLine +
+                    "-HttpStatus: {1}" + Environment.NewLine +
+                    "-Reason Phrase: {2}" + Environment.NewLine +
+                    "-ContentLength: {3:0.00 KB}" + Environment.NewLine +
+                    "-Contents: " + Environment.NewLine + "{4}" + Environment.NewLine +
+                    "---------------------------------",
+                    response.RequestMessage.RequestUri.OriginalString,
+                    string.Format("{0} {1}", (int)response.StatusCode, response.StatusCode.ToString()),
+                    response.ReasonPhrase,
+                    Convert.ToDecimal(Convert.ToDouble(response.Content.Headers.ContentLength) / 1024),
+                    response.Content?.ReadAsStringAsync().Result
+                    );
+                this.Log(message);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error during Log(HttpResponseMessage request): " + ex.ToString());
+            }
         }
 
         #endregion
